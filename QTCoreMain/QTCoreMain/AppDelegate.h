48a7d87aec64605d23608b72a65283cd5f3c53f4
MasterBie <>
2018-10-18 16:08:36 +0800

    项目初始化

---
+++ QTCoreMain/QTCoreMain/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QTCoreMain
+//
+//  Created by MasterBie on 2018/10/18.
+//  Copyright © 2018年 MasterBie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QTCoreMain/QTCoreMain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QTCoreMain
+//
+//  Created by MasterBie on 2018/10/18.
+//  Copyright © 2018年 MasterBie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
