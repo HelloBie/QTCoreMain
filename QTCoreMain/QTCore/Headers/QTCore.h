19dac26fc6a484e14eed04b0318cd6d3f30a6159
不不不紧张 <1005903848@qq.com>
2020-07-16 12:41:46 +0800

    测试

--- QTCoreMain/QTCore/Headers/QTCore.h
+++ QTCoreMain/QTCore/Headers/QTCore.h
@@ -5,20 +5,20 @@
 //  Created by bqt on 2017/7/25.
 //  Copyright © 2017年 bambootech. All rights reserved.
 //
-#import "UIView+Setup.h"
-#import "UITextField+Setup.h"
-#import "UIImageView+Setup.h"
-#import "UIButton+Setup.h"
-#import "UILabel+Setup.h"
-#import "UIScrollView+Setup.h"
-
-
-#import "UIImage+Tool.h"
-#import "UIButton+AutoFontSize.h"
-#import "UserModel.h"
-#import "NetWorkingTool.h"
-#import "NSString+MD5.h"
-#import "UIImage+Tool.h"
+//#import "UIView+Setup.h"
+//#import "UITextField+Setup.h"
+//#import "UIImageView+Setup.h"
+//#import "UIButton+Setup.h"
+//#import "UILabel+Setup.h"
+//#import "UIScrollView+Setup.h"
+//
+//
+//#import "UIImage+Tool.h"
+//#import "UIButton+AutoFontSize.h"
+//#import "UserModel.h"
+//#import "NetWorkingTool.h"
+//#import "NSString+MD5.h"
+//#import "UIImage+Tool.h"
 #import "UIViewController+HUD.h"
 
 #ifndef QTCore_h
